f4fd009ee1474e7ba0287c293b89fd8288dcaacd
dev-6 <tryus.dev6@gmail.com>
2023-07-30 15:09:19 +0600

    new update2

--- app/src/main/java/com/example/jetpackcomponent/randomUser/UserMainActivity.kt
+++ app/src/main/java/com/example/jetpackcomponent/randomUser/UserMainActivity.kt
@@ -1,30 +1,48 @@
 package com.example.jetpackcomponent.randomUser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jetpackcomponent.R
+import com.example.jetpackcomponent.connectivity.NetworkConnectivity
 import com.example.jetpackcomponent.randomUser.model.Result
 
 class UserMainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView : RecyclerView
     private lateinit var userAdapter: UserAdapter
-    private lateinit var userList : List<Result>
+    private lateinit var userList : ArrayList<Result>
     private lateinit var userViewModel: UserViewModel
+    private lateinit var networkConnectivity: NetworkConnectivity
+    private lateinit var networkText : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_main)
 
-        //userList = ArrayList()
+        userList = ArrayList()
 
         recyclerView = findViewById(R.id.userRecView)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        userAdapter = UserAdapter(userList, this)
+        recyclerView.adapter = userAdapter
+        networkText = findViewById(R.id.networkState)
+
+        networkConnectivity = NetworkConnectivity(application)
+
+        networkConnectivity.observe(this, Observer { isAvailable ->
+
+            when(isAvailable)
+            {
+                true-> networkText.text = "Connction Avilable"
+                else -> networkText.text = "Connection Lost"
+            }
+
+        })
 
 
         userViewModel = ViewModelProvider(this, UserViewModelFactory(
@@ -36,13 +54,13 @@ class UserMainActivity : AppCompatActivity() {
         userViewModel.users.observe(this, Observer {
 
             val userInfo = it.results
-            userList = userInfo
+//            userList = userInfo
+            userList.clear()
+            userList.addAll(userInfo)
 
             // Initialize the adapter after receiving the data
-            userAdapter = UserAdapter(userList, this)
-            recyclerView.adapter = userAdapter
 
-            Toast.makeText(this, ""+userList.size, Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, ""+userList.size, Toast.LENGTH_SHORT).show()
 
             // Notify the adapter that data has changed (if needed)
             userAdapter.notifyDataSetChanged()
